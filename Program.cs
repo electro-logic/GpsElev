203767e5202608d384b0a448d67ed52f899c43fc
user <user@noreply.com>
2023-05-04 23:28:04 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿// Extract GPS Altitude from the GPS timestamp and write it as metadata
+// exiftool (https://exiftool.org/) executable is required in the same folder of this app
+using System.Diagnostics;
+var files = Directory.EnumerateFiles(Environment.CurrentDirectory, "*.arw");
+foreach (var file in files)
+{
+    // Extract timestamp (example: "02:04:50")
+    string timestamp = ExifTool($"{file} -gpstimestamp -T");
+    // Calculate GPS Altitude  Base 60 encoded from Time
+    var tokens = timestamp.Split(":");
+    if (tokens.Length != 3)
+    {
+        Console.WriteLine($"{file} skipped");
+        continue;
+    }
+    double altitude = (double.Parse(tokens[0]) * 3600 + double.Parse(tokens[1]) * 60 + double.Parse(tokens[2])) / 10.0;
+    // Write exif Altitude (meters), AltitudeRef (above sea level), Remove Timestamp
+    ExifTool($"-GPSAltitude={altitude} -GPSAltitudeRef=0 -GPSMeasureMode=3 -GPSTimeStamp= {file}");
+    Console.WriteLine($"EXIF Metadata updated for {file}");
+}
+string ExifTool(string arg)
+{
+    var psi = new ProcessStartInfo("exiftool.exe", arg)
+    {
+        RedirectStandardOutput = true,
+        RedirectStandardError = true,
+        RedirectStandardInput = true
+    };
+    var proc = Process.Start(psi);
+    proc.StandardInput.WriteLine();
+    proc.WaitForExit(5000);
+    string result = proc.StandardOutput.ReadToEnd();
+    return result;
+}
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # GpsElev
+
+This small utility convert the GPS Altitude encoded in the GPS timestamp metadata
+
+# Why this utility
+
+Sony mirrorless camera (ex. A6400, A7, etc..) write limited GPS metadata: latitude, longitude and timestamp to images when Location Information Linkage is enabled. Unfortunately the altitude is missing.
+
+To overcome this limitation a custom app can send a manipulated timestamp containing the altitude information, this app will take care of putting the altitude data in the right metadata tag.
+
+# How this works
+
+GPS Timestamp is composed by 3 numbers in base 60, for example 02:04:50
+
+The altitude information from the GPS/GNNS is usually not very accurated, so a decimal digit only is used.
+
+The raw altitude is scaled by 10 and then converted from base 10 to base 60.
+
+The maximum storable altitude is 60*60*60/10=21600 meters, that's enough for most applications.
+
+# Requirements
+
+.NET 7
+
+Exiftool
